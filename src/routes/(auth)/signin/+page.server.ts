d71e663adb1b8da1295d0eaf0edcdf7eb10c87b8
egouinguenet <elouangouinguenet@gmail.com>
2025-06-13 10:56:31 -0400

    lazy ass fuck filling for dev mode

--- src/routes/(auth)/signin/+page.server.ts
+++ src/routes/(auth)/signin/+page.server.ts
@@ -2,8 +2,13 @@ import { superValidate } from "sveltekit-superforms";
 import { zod } from "sveltekit-superforms/adapters";
 import type { PageServerLoad } from "./$types";
 import { formSchema } from "./schema";
+import { dev } from '$app/environment';
 
 export const load: PageServerLoad = async () => {
+  if (dev) {
+    return { form: await superValidate({ email: "test@example.com", password: "qwerqwerqwer" }, zod(formSchema)) }
+  }
+
   return {
     form: await superValidate(zod(formSchema))
   }
