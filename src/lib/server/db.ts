057c954e1cdb03d238edc1546da2918ee1f4e86d
egouinguenet <elouangouinguenet@gmail.com>
2025-06-16 21:09:31 -0400

    fix: update drizzle db creation by providing schema to enable queries and typesafety

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -2,7 +2,8 @@ import { DATABASE_URL } from "$env/static/private";
 import { neon } from "@neondatabase/serverless";
 import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from '$lib/server/schema'
 
 config({ path: ".env" })
 const sql = neon(DATABASE_URL)
-export const db = drizzle({ client: sql })
+export const db = drizzle({ client: sql, schema: schema })
