537e6fc5396b803b3249607d89f9f460f9dbd5ae
egouinguenet <elouangouinguenet@gmail.com>
2025-06-17 20:11:09 -0400

    feat: wrapped primary nav bar into anchor tag

--- src/lib/components/nav-main.svelte
+++ src/lib/components/nav-main.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import { SidebarMenuButton } from '$lib/components/ui/sidebar/index.js';
 	import type { Icon } from '@tabler/icons-svelte';
 
 	let { items }: { items: { title: string; url: string; icon?: Icon }[] } = $props();
@@ -11,10 +12,16 @@
 			{#each items as item (item.title)}
 				<Sidebar.MenuItem>
 					<Sidebar.MenuButton tooltipContent={item.title}>
-						{#if item.icon}
-							<item.icon />
-						{/if}
-						<span>{item.title}</span>
+						{#snippet child({ ...props })}
+							<a href={item.url}>
+								<SidebarMenuButton {...props}>
+									{#if item.icon}
+										<item.icon />
+									{/if}
+									<span>{item.title}</span>
+								</SidebarMenuButton>
+							</a>
+						{/snippet}
 					</Sidebar.MenuButton>
 				</Sidebar.MenuItem>
 			{/each}
